328fe14b4de089e92cf99ca507448c4652b8cd75
Luis Enrique <intendentegood@gmail.com>
2022-07-04 10:49:50 -0300

    IIF04hecho

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,20 @@
+let edad;
+//INICIO DE FUNCION PARA VER SI ES ADOLESCENTE O NO
+function esAdolescente(){
+	//tomo la edad  
+	edad = document.getElementById("txtIdEdad").value;
+	if(edad>0){
+	   if(edad>=13 && edad<=17){
+		  return alert(`Es Adolescente Tiene ${edad} años`);
+	   }
+	   return ;
+	}
+	return alert(`*C asusta*`);
+}
+//FIN DE LA FUNCIÓN
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+  esAdolescente();
 
-
-}//FIN DE LA FUNCIÓN
+}
