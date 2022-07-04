b73cb8d2ad9d6f92405633089f52c63ed222df4b
Luis Enrique <intendentegood@gmail.com>
2022-07-04 13:06:37 -0300

    IFF06hecho

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,24 @@
+let edad;
+//INICIO DE LA FUNCION PARA VERIFICAR LA EDAD
+function esMayorAdolescenteOniño(){
+	//tomo la edad  
+	edad = document.getElementById("txtIdEdad").value;
+	if(edad>0){
+	   if(edad>=18){
+		  return alert(`La Persona Es Mayor De Edad Tiene ${edad} años`);
+	   }
+	       if(edad>=13 && edad<=17){
+		        return alert(`Es Adolescente Tiene ${edad} años`);
+	       }
+		   else {
+			return alert(`Es un niño su edad es ${edad}`);
+		   }
+    }
+	return alert(`*C asusta*`)
+}
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-
-
+	esMayorAdolescenteOniño();
 
 }//FIN DE LA FUNCIÓN
