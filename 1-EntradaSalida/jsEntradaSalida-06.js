9fbe844c5851e617be006554015ed33ebb2ffe56
Luis Enrique <intendentegood@gmail.com>
2022-07-04 01:45:31 -0300

    EyS06hecho

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,13 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+let num1
+let num2
+var tYsumar
+num1 = parseInt(document.getElementById('txtIdNumeroUno').value);
+num2 = parseInt(document.getElementById('txtIdNumeroDos').value);
+tYsumar = num1 + num2;
+
+	alert(tYsumar);
 }
 
