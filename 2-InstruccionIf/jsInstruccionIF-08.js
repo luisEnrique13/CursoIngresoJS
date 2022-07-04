45644ef3a4626d981106ae0faca0f84dd3d21767
Luis Enrique <intendentegood@gmail.com>
2022-07-04 15:34:07 -0300

    IFF08hecho

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,24 @@
+
+let edad;
+let estadocivil;
+
+function verificarEdadYestadoCivil(){
+	estadocivil = estadocivil = document.getElementById("estadoCivil").value;
+	edad = document.getElementById("txtIdEdad").value;
+	    if(edad<18 && estadocivil!="Soltero"){
+		return ;
+	    }
+		
+	      else{
+		     	if(edad>=18 && estadocivil=="Soltero"){
+			  	return alert("Es soltero y no es menor");
+		    	}
+			}
+
+}
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	verificarEdadYestadoCivil();
 
-
-}//FIN DE LA FUNCIÓN
+}
