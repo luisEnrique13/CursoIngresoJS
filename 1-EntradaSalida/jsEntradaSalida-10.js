acbab5c87f6045d2374e40afe13e414af27b624b
Luis Enrique <intendentegood@gmail.com>
2022-07-04 09:41:40 -0300

    EyS10hecho

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,7 +3,14 @@ Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
+let importe;
+let aumento;
+let resultado;
+
 function mostrarAumento()
 {
-	alert("ok");
+	importe = parseInt(document.getElementById('txtIdImporte').value);
+    aumento = (importe*25)/100;
+	resultado = parseInt(document.getElementById('txtIdResultado').value = importe-aumento); 
+	alert(`El importe con 25% de descuento es ${resultado}`);
 }
