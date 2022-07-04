252e4c0b28afa1d8d08eecee837689a3670fefcb
Luis Enrique <intendentegood@gmail.com>
2022-07-04 10:25:46 -0300

    IIF03hecho

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,21 @@
+let edad;
+//INICIO DE LA FUNCION PARA VERIFICAR LA EDAD
+function esMayorDeEdadONo(){
+	//tomo la edad  
+	edad = document.getElementById("txtIdEdad").value;
+	if(edad>0){
+	   if(edad>=18){
+		  return alert(`La Persona Es Mayor De Edad Tiene ${edad} años`);
+	   }
+	      return alert(`La Persona Es Menor De Edad Tiene ${edad} años`);
+    }
+	return alert(`*C asusta*`)
+}
+//FIN DE LA FUNCIÓN
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//Ejecuto la funcion esMayorDeEdadONo, que verifica la edad
+	esMayorDeEdadONo();
 
-}//FIN DE LA FUNCIÓN
+}
