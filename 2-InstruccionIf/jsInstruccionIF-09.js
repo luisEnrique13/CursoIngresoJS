9d4486113aecf1e75c9219ded1250122a302f77c
Luis Enrique <intendentegood@gmail.com>
2022-07-05 10:08:28 -0300

    IFF09hecho

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,12 @@
+function numeroRandomEntre(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	alert(`el numero que te salio es ${numeroRandomEntre(1,10)}`);	
 
 }//FIN DE LA FUNCIÓN
