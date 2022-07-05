62348d73eb1a3880573c783a26112360901eee3f
Luis Enrique <intendentegood@gmail.com>
2022-07-05 10:25:06 -0300

    IFF10hecho

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,28 @@
+function numeroRandomEntre(min, max) {
+	//genero el número RANDOM entre x y x 
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+function verificarNota(){
+	nota = numeroRandomEntre(0,10);
+	if(nota>=9){
+		return alert (`EXCELENTE, su nota es ${nota}`);
+	}
+	    if ( nota>=4 && nota<9){
+	        return alert(`APROBO, su nota es ${nota}`);
+		}
+		  else {   if(nota<4){
+		             return alert(`Vamos, la proxima se puede, su nota es ${nota}`);
+		           } 
+	           }
+}
+
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	
+ verificarNota();
 
-}//FIN DE LA FUNCIÓN
+}
+//FIN DE LA FUNCIÓN
