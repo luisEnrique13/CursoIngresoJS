437c6ddf7bb22d57de1b3cb22d5c8abc6dd64837
Luis Enrique <intendentegood@gmail.com>
2022-07-07 12:02:31 -0300

    TP2hecho

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -3,13 +3,14 @@ A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el anch
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
+let perimetro;
+let alambre;
+let ancho;
+let largo;
 
 function Rectangulo () 
 {
-    let perimetro;
-    let ancho;
-    let largo;
-    let alambre;
+   
     largo =parseInt(document.getElementById("txtIdLargo").value);
     ancho =parseInt(document.getElementById("txtIdAncho").value);
     perimetro = (ancho + largo)*2;
@@ -20,32 +21,27 @@ function Rectangulo ()
 }
 function Circulo () 
 {
-
-    let perimetro;
     let radio;
-    let alambre;
     radio = parseInt(document.getElementById("txtIdRadio").value);
     perimetro = 2 * Math.PI * radio;
     alambre = perimetro *3;
 
-    alert(`se necesita comprar ${alambre} metros de alambre`);
+    alert(`se necesita comprar ${alambre.toFixed(2)} metros de alambre`);
 }
 function Materiales () 
 {
 
- let largo;
- let ancho;
- let area;
- let cemento;
- let cal;
+    let area;
+    let cemento;
+    let cal;
 
- largo =parseInt(document.getElementById("txtIdLargo").value);
- ancho =parseInt(document.getElementById("txtIdAncho").value);
- area = largo * ancho;
- cemento = area * 2;
- cal = area * 3 ;
+    largo =parseInt(document.getElementById("txtIdLargo").value);
+    ancho =parseInt(document.getElementById("txtIdAncho").value);
+    area = largo * ancho;
+    cemento = area * 2;
+    cal = area * 3 ;
 
- alert(`Se necesitan ${cemento} bolsas de cemento y ${cal} bolsas de cal`);
+    alert(`Se necesitan ${cemento} bolsas de cemento y ${cal} bolsas de cal`);
 
  
  
