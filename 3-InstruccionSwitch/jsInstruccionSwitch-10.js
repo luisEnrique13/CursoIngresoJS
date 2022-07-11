1d67220872559a2dfdc85847346d748d5149873b
Luis Enrique <intendentegood@gmail.com>
2022-07-11 14:17:37 -0300

    Switch10Hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,41 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacion =document.getElementById("txtIdEstacion").value;
+    let destino =document.getElementById("txtIdDestino").value;
 
+
+	switch(estacion){
+		case "Invierno":
+		 	if(destino=="Bariloche"){
+			 	alert("Se viaja");
+			 }
+					 else{
+						 alert("No se viaja");
+					}
+				 break;
+
+ 		case "Verano":
+			 if(destino=="Cataratas" || destino == "Mar del plata"){
+			 	alert("Se viaja");
+			 } 
+			 		 else{
+						alert("No se viaja")
+					}
+				 break;
+			 
+
+ 		case "Primavera":
+			 if(destino =="Bariloche"){
+				alert("No se viaja")
+			 }
+			 		 else{
+						 alert("se viaja")
+					}
+				 break;
+
+ 		case "Otoño":
+			 alert("Se viaja");
+				 break;
+
+ } 
 }//FIN DE LA FUNCIÓN
