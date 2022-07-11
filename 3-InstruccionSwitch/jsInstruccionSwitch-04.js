cd8fc7499302455a6002c2225564ba60c83b9be7
Luis Enrique <intendentegood@gmail.com>
2022-07-11 05:29:16 -0300

    Switch4Hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,18 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let mes =document.getElementById("txtIdMes").value;
+	switch(mes){
+		case "Febrero" : alert("tiene 28 dias");
+	    break; 
 
+		case "Abril" :  
+		case "Septiembre" : 
+		case "Noviembre" :
+		case "Junio" : alert("tiene 30 dias");
+		break;
 
+		default: alert("tiene 31 dias");	
+	}
 
 }//FIN DE LA FUNCIÓN
