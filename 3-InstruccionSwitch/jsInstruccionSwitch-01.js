fda020d139c4af5965f374090e1cc9795a4248f9
Luis Enrique <intendentegood@gmail.com>
2022-07-11 05:06:35 -0300

    Switch1Hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,21 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes = document.getElementById("txtIdMes").value;
+	
+	switch(mes){
+        case "Enero": alert( "Que comiences bien el año!!!.");
+	    break;
+		case "Marzo": alert( "a clases!!!.");
+	    break;
+		case "Julio": alert( "se vienen las vacaciones!!!.");
+	    break;
+		case "Diciembre": alert( "Felices fiesta!!!.");
+	    break;
 
 
+	}
+
+ 
 
 }//FIN DE LA FUNCIÓN
