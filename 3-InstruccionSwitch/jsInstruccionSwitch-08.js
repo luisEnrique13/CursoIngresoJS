8b4d489510f65ccaf2448ee1ca8d8b58f90bd89c
Luis Enrique <intendentegood@gmail.com>
2022-07-11 13:07:04 -0300

    Switch8Hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,19 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino = document.getElementById("txtIdDestino").value;
+	
+	switch(destino){
+
+		case "Bariloche": 
+		case "Ushuaia":
+			alert("Hace frio");
+				break; 
+
+		case "Mar del plata":
+		case "Cataratas":
+			alert("Hace calor");
+				break;		
+
+	}
 
 }//FIN DE LA FUNCIÓN
