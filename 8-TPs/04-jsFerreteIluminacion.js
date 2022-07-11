b837b74ce1c7ab74e6d45e928a057549dff0c72c
Luis Enrique <intendentegood@gmail.com>
2022-07-11 08:46:45 -0300

    TP4hecho

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -8,7 +8,72 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
+
+ const PRECIO = 35;
+ let descuento;
+ let precioConDescuento;
+ let importeFinal;
+ let cantidad; 
+ let marca;
+ let iibb;
+
 function CalcularPrecio () 
-{
- 	
+{    
+    marca = document.getElementById("Marca").value;
+    cantidad = parseInt(document.getElementById("txtIdCantidad").value);
+
+    switch(cantidad){
+        case 1:  
+        case 2: descuento = 0;
+            break;
+
+        case 3:
+            if(marca == "ArgentinaLuz"){
+                descuento = 15;
+            }
+                else if(marca == "FelipeLamparas") {
+                    descuento = 10;
+                 }
+                   else {
+                     descuento  = 5;
+                   }
+            break; 
+
+        case 4:
+            if(marca== "ArgentinaLuz" || marca == "FelipeLamparas"){
+                descuento = 25;
+            }
+               else {
+                     descuento = 20;
+                }
+            break;
+
+        case 5:  
+             if(marca == "ArgentinaLuz"){
+                descuento = 40;
+             }
+                else{
+                    descuento = 30;
+               }
+            break; 
+        
+        default : descuento = 50;
+               
+    }
+
+    precioConDescuento = PRECIO - PRECIO * descuento /100;
+    document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
+
+     importeFinal= precioConDescuento * cantidad;
+
+    if(importeFinal>120){
+        iibb = importeFinal*(10/100);
+        importeFinal +=  iibb;
+        alert(`El precio final es $ ${importeFinal} junto a $ ${iibb} de Ingresos Brutos`);
+    }
+    else {
+        alert(`total $ ${importeFinal}`);
+    }
+
+    
 }
